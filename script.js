fd8d8046e2adea55908c7acfc15acb6579352b17
Justin L <justinliang082@gmail.com>
2025-03-04 23:39:06 -0500

    script.js

--- script.js
+++ script.js
@@ -5,3 +5,32 @@ const out = document.getElementById('output1');
 btn.addEventListener('click', () => {
 out.insertAdjacentHTML('beforeend', `<li>${txt.value}</li>`);
 });
+
+function removeAll() {
+    document.getElementById("output1").innerHTML="";
+}
+
+function change() {
+    var select = document.getElementById("select").value;
+    document.getElementById("output").innerHTML = select;
+}
+
+function change2() {
+    var select = document.getElementById("select2").value;
+    document.getElementById("output2").innerHTML = select;
+}
+
+function change3() {
+    var select = document.getElementById("select3").value;
+    document.getElementById("output3").innerHTML = select;
+}
+
+function change4() {
+    var select = document.getElementById("select4").value;
+    document.getElementById("output4").innerHTML = select;
+}
+
+function change5() {
+    var select = document.getElementById("select5").value;
+    document.getElementById("output5").innerHTML = select;
+}
